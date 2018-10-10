8d75b6768e80eaefd4e118f8974a73f0923f98ab
kgitbank <kgitbank@DESKTOP-U6AB831>
2018-10-10 15:20:19 +0900

    ing

--- src/app/model/EmployeeDao.java
+++ src/app/model/EmployeeDao.java
@@ -1,8 +1,19 @@
 package app.model;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class EmployeeDao {
+	
+	public List<Map> getDeparments(){
+		return null;
+	}
+	
+	public List<Map> getPositions(){
+		return null;
+	}
 	
 }
